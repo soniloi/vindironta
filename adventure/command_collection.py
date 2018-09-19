4a9cbefe447ad8d8d8f219b4b27d52ab22e9a871
soniloi <niloinso@tcd.ie>
2018-09-19 18:56:20 +0100

    Tidy command parsing code

--- adventure/command_collection.py
+++ adventure/command_collection.py
@@ -14,19 +14,33 @@ class CommandCollection:
 	def create_command(self, line):
 		tokens = line.split("\t")
 
-		command_id = tokens[0]
-		command_attributes = int(tokens[1], 16)
-		command_function_name = "handle_" + tokens[2]
-		command_function = getattr(self, command_function_name, None)
+		command_id = self.parse_command_id(tokens[0])
+		command_attributes = self.parse_command_attributes(tokens[1])
+		command_function = self.parse_command_function(tokens[2])
 
 		if command_function:
-			command_names = tokens[3].split(",")
-			primary_command_name = command_names[0]
-
+			(primary_command_name, command_names) = self.parse_command_names(tokens[3])
 			command = Command(command_id, command_attributes, command_function, primary_command_name)
-
 			for command_name in command_names:
 				self.commands[command_name] = command
+
+
+	def parse_command_id(self, token):
+		return int(token)
+
+
+	def parse_command_attributes(self, token):
+		return int(token, 16)
+
+
+	def parse_command_function(self, token):
+		command_function_name = "handle_" + token
+		return getattr(self, command_function_name, None)
+
+
+	def parse_command_names(self, token):
+		command_names = token.split(",")
+		return (command_names[0], command_names)
 
 
 	def get(self, name):
