d757470fe5c6b1bbadaf195ec2b68bfd97698be8
soniloi <niloinso@tcd.ie>
2018-09-21 22:30:49 +0100

    Changed how Item containers are initialized

--- adventure/item.py
+++ adventure/item.py
@@ -2,9 +2,9 @@ from adventure.data_element import DataElement
 
 class Item(DataElement):
 
-	def __init__(self, item_id, attributes, shortname, longname, description, initial_container, size, writing):
+	def __init__(self, item_id, attributes, shortname, longname, description, size, writing):
 		super().__init__(data_id=item_id, attributes=attributes, shortname=shortname, longname=longname,
 			description=description)
-		self.container = initial_container
 		self.size = size
 		self.writing = writing
+		self.container = None
--- adventure/item_collection.py
+++ adventure/item_collection.py
@@ -8,20 +8,22 @@ class ItemCollection:
 
 	def __init__(self, reader, location_collection):
 		self.items = {}
+		item_containers = {}
+
 		line = reader.read_line()
 		while not line.startswith("---"):
-			self.create_item(line, location_collection)
+			self.create_item(line, item_containers)
 			line = reader.read_line()
 
+		self.place_items(item_containers, location_collection)
 
-	def create_item(self, line, location_collection):
+
+	def create_item(self, line, item_containers):
 		tokens = line.split("\t")
 
 		item_id = self.parse_item_id(tokens[0])
 		item_attributes = self.parse_item_attributes(tokens[1])
-
-		# TODO: this may not be a location
-		item_container = self.parse_item_location(tokens[2], location_collection)
+		item_container_id = self.parse_item_container_id(tokens[2])
 		item_size = self.parse_item_size(tokens[3])
 		item_primary_shortname, item_shortnames = self.parse_item_shortnames(tokens[4])
 		item_longname = tokens[5]
@@ -34,17 +36,14 @@ class ItemCollection:
 			shortname = item_primary_shortname,
 			longname = item_longname,
 			description = item_description,
-			initial_container = item_container,
 			size = item_size,
 			writing = item_writing,
 		)
 
-		# TODO: location may not be a Location, but could also be inventory, another item, etc.
-		if item_container:
-			item_container.insert_item(item)
-
 		for item_shortname in item_shortnames:
 			self.items[item_shortname] = item
+
+		item_containers[item] = item_container_id
 
 
 	def parse_item_id(self, token):
@@ -55,8 +54,8 @@ class ItemCollection:
 		return int(token, 16)
 
 
-	def parse_item_location(self, token, location_collection):
-		return location_collection.get(int(token))
+	def parse_item_container_id(self, token):
+		return int(token)
 
 
 	def parse_item_size(self, token):
@@ -72,6 +71,15 @@ class ItemCollection:
 		if token == ItemCollection.NO_WRITING:
 			return None
 		return token
+
+
+	# TODO: handle initial placement types other than Location
+	def place_items(self, item_containers, location_collection):
+		for item, container_id in item_containers.items():
+			container = location_collection.get(container_id)
+			if container:
+				item.container = container
+				container.insert_item(item)
 
 
 	def get(self, item_name):
