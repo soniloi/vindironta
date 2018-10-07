e13bddf2dfe03cacd45ca9ecd55f84987ce8dfc5
soniloi <niloinso@tcd.ie>
2018-10-07 12:31:04 +0100

    Improve testing coverage and structure for CommandCollection

--- adventure/test/test_command_collection.py
+++ adventure/test/test_command_collection.py
@@ -1,5 +1,5 @@
 import unittest
-from unittest.mock import patch
+from unittest.mock import Mock
 
 from adventure.argument_resolver import ArgumentResolver
 from adventure.command import Command
@@ -12,45 +12,116 @@ from adventure.player import Player
 class TestCommandCollection(unittest.TestCase):
 
 	def setUp(self):
-		with patch(file_reader.__name__ + ".FileReader") as reader_mock:
-			reader_mock_instance = reader_mock.return_value
-			reader_mock_instance.read_line.side_effect = [
-				"33\t80\tlook\tlook,l",
-				"50\t0\tscore\tscore",
-				"81\t10\tignore\tignore",
-				"16\t40\tgo\teast,e",
-				"56\t0C\ttake\ttake",
-				"1000\t0\tnotacommand\tnotacommand",
-				"---\t\t\t",
-			]
-
-			self.collection = CommandCollection(reader_mock_instance, ArgumentResolver(), CommandHandler())
+		self.argument_resolver = ArgumentResolver()
+		self.command_handler = CommandHandler()
 
 
-	def test_init_commands(self):
-		self.assertEqual(7, len(self.collection.commands))
-		self.assertTrue("look" in self.collection.commands)
-		self.assertTrue("l" in self.collection.commands)
-		self.assertTrue("score" in self.collection.commands)
-		self.assertTrue("ignore" in self.collection.commands)
-		self.assertTrue("e" in self.collection.commands)
-		self.assertTrue("east" in self.collection.commands)
-		self.assertTrue("take" in self.collection.commands)
+	def test_init_command_different_commands(self):
+		reader_mock = Mock()
+		reader_mock.read_line.side_effect = [
+			"50\t0\tscore\tscore",
+			"81\t10\tlook\tlook",
+			"---\t\t\t",
+		]
 
-		score_command = self.collection.commands["score"]
-		look_command = self.collection.commands["look"]
-		l_command = self.collection.commands["l"]
-		east_command = self.collection.commands["east"]
-		take_command = self.collection.commands["take"]
+		collection = CommandCollection(reader_mock, self.argument_resolver, self.command_handler)
 
+		self.assertEqual(2, len(collection.commands))
+		self.assertTrue("score" in collection.commands)
+		self.assertTrue("look" in collection.commands)
+
+		score_command = collection.commands["score"]
+		look_command = collection.commands["look"]
 		self.assertIsNot(score_command, look_command)
+
+
+	def test_init_command_aliases(self):
+		reader_mock = Mock()
+		reader_mock.read_line.side_effect = [
+			"33\t80\tlook\tlook,l",
+			"---\t\t\t",
+		]
+
+		collection = CommandCollection(reader_mock, self.argument_resolver, self.command_handler)
+
+		self.assertEqual(2, len(collection.commands))
+		self.assertTrue("look" in collection.commands)
+		self.assertTrue("l" in collection.commands)
+
+		look_command = collection.commands["look"]
+		l_command = collection.commands["l"]
 		self.assertIs(look_command, l_command)
 
 
-	def test_list_commands(self):
-		result = self.collection.list_commands()
+	def test_init_non_existent_command(self):
+		reader_mock = Mock()
+		reader_mock.read_line.side_effect = [
+			"1000\t0\tnotacommand\tnotacommand",
+			"---\t\t\t",
+		]
 
-		self.assertEqual("e/east, l/look, score, take", result)
+		collection = CommandCollection(reader_mock, self.argument_resolver, self.command_handler)
+
+		self.assertEqual(0, len(collection.commands))
+
+
+	def test_init_movement_command(self):
+		reader_mock = Mock()
+		reader_mock.read_line.side_effect = [
+			"16\t40\tgo\teast",
+			"---\t\t\t",
+		]
+
+		collection = CommandCollection(reader_mock, self.argument_resolver, self.command_handler)
+
+		self.assertTrue("east" in collection.commands)
+		east_command = collection.commands["east"]
+		self.assertEqual(self.argument_resolver.resolve_movement, east_command.resolver_function)
+
+
+	def test_init_argless_command(self):
+		reader_mock = Mock()
+		reader_mock.read_line.side_effect = [
+			"50\t0\tscore\tscore",
+			"---\t\t\t",
+		]
+
+		collection = CommandCollection(reader_mock, self.argument_resolver, self.command_handler)
+
+		self.assertTrue("score" in collection.commands)
+		score_command = collection.commands["score"]
+		self.assertEqual(self.argument_resolver.resolve_argless, score_command.resolver_function)
+
+
+	def test_init_single_arg_command(self):
+		reader_mock = Mock()
+		reader_mock.read_line.side_effect = [
+			"56\t0C\ttake\ttake",
+			"---\t\t\t",
+		]
+
+		collection = CommandCollection(reader_mock, self.argument_resolver, self.command_handler)
+
+		self.assertTrue("take" in collection.commands)
+		take_command = collection.commands["take"]
+		self.assertEqual(self.argument_resolver.resolve_single_arg, take_command.resolver_function)
+
+
+	def test_list_commands(self):
+		reader_mock = Mock()
+		reader_mock.read_line.side_effect= [
+			"33\t80\tlook\tlook,l",
+			"50\t0\tscore\tscore",
+			"81\t10\tlook\tlook",
+			"16\t40\tgo\teast,e",
+			"56\t0C\ttake\ttake",
+			"1000\t0\tnotacommand\tnotacommand",
+			"---\t\t\t",
+		]
+
+		collection = CommandCollection(reader_mock, self.argument_resolver, self.command_handler)
+
+		self.assertEqual("e/east, l/look, score, take", collection.list_commands())
 
 
 if __name__ == "__main__":
