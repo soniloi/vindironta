dd31935e25787b9962aa9d167b352d8b60a9e930
soniloi <niloinso@tcd.ie>
2018-10-13 14:24:21 +0100

    Rewrite tests for ItemCollection

--- adventure/test/test_item_collection.py
+++ adventure/test/test_item_collection.py
@@ -1,5 +1,5 @@
 import unittest
-from unittest.mock import patch
+from unittest.mock import patch, Mock
 
 from adventure.item import ContainerItem
 from adventure.item_collection import ItemCollection
@@ -10,28 +10,13 @@ from adventure.location import Location
 class TestItemCollection(unittest.TestCase):
 
 	def setUp(self):
-		with patch(file_reader.__name__ + ".FileReader") as reader_mock, \
-			patch(location_collection.__name__ + ".LocationCollection") as location_collection_mock:
-			reader_mock_instance = reader_mock.return_value
-			reader_mock_instance.read_line.side_effect = [
-				"1042\t2002\t27\t3\tkohlrabi,cabbage\tsome kohlrabi\tsome kohlrabi, or Brassica oleracea var. gongylodes, a cabbage cultivar\t0",
-				"1076\t22802\t1007\t1\twater\twater\tRiver Amethyst water. It is cold and clear\t0",
-				"1105\t2\t80\t2\tbook\ta book\ta book of fairytales in English. It is open on a particular page\tThe Pied Piper of Hamelin",
-				"1002\t3\t119\t5\tbasket\ta\tbasket\ta\tlarge basket\t0",
-				"---",
-			]
+		self.location_collection = Mock()
+		self.location_collection.get.side_effect = self.container_side_effect
 
-			location_collection_mock_instance = location_collection_mock.return_value
-			location_collection_mock_instance.get.side_effect = self.container_side_effect
-
-			self.kohlrabi_initial_container = Location(0, 0, "House", "in a house", " with a red door")
-			self.book_initial_container = Location(1, 1, "Library", "in the Library", ", a tall, bright room")
-			self.container_map = {
-				27 : self.kohlrabi_initial_container,
-				80 : self.book_initial_container,
-			}
-
-			self.collection = ItemCollection(reader_mock_instance, location_collection_mock_instance)
+		self.book_initial_container = Location(1, 1, "Library", "in the Library", ", a tall, bright room")
+		self.container_map = {
+			80 : self.book_initial_container,
+		}
 
 
 	def container_side_effect(self, *args):
@@ -41,14 +26,18 @@ class TestItemCollection(unittest.TestCase):
 		return None
 
 
-	def test_init(self):
-		self.assertEqual(5, len(self.collection.items))
-		self.assertTrue("book" in self.collection.items)
-		self.assertTrue("cabbage" in self.collection.items)
-		self.assertTrue("kohlrabi" in self.collection.items)
-		self.assertTrue("basket" in self.collection.items)
+	def test_init_single_item(self):
+		reader_mock = Mock()
+		reader_mock.read_line.side_effect = [
+			"1105\t0x2\t80\t2\tbook\ta book\ta book of fairytales in English. It is open on a particular page\tThe Pied Piper of Hamelin",
+			"---",
+		]
 
-		book = self.collection.items["book"]
+		collection = ItemCollection(reader_mock, self.location_collection)
+
+		self.assertEqual(1, len(collection.items))
+		self.assertTrue("book" in collection.items)
+		book = collection.items["book"]
 		self.assertEqual(0x2, book.attributes)
 		self.assertEqual(self.book_initial_container, book.container)
 		self.assertEqual(2, book.size)
@@ -57,27 +46,68 @@ class TestItemCollection(unittest.TestCase):
 		self.assertEqual("a book of fairytales in English. It is open on a particular page", book.description)
 		self.assertEqual("The Pied Piper of Hamelin", book.writing)
 		self.assertFalse(isinstance(book, ContainerItem))
-
-		kohlrabi = self.collection.items["kohlrabi"]
-		self.assertEqual(0x2002, kohlrabi.attributes)
-		self.assertEqual(self.kohlrabi_initial_container, kohlrabi.container)
-		self.assertEqual("kohlrabi", kohlrabi.shortname)
-		self.assertIsNone(kohlrabi.writing)
-		self.assertFalse(isinstance(kohlrabi, ContainerItem))
-
-		cabbage = self.collection.items["cabbage"]
-		self.assertIs(kohlrabi, cabbage)
-		self.assertIsNot(kohlrabi, book)
-
-		water = self.collection.items["water"]
-		self.assertIsNone(water.container)
-		self.assertFalse(isinstance(water, ContainerItem))
-
-		basket = self.collection.items["basket"]
-		self.assertTrue(isinstance(basket, ContainerItem))
-
 		self.assertEqual(book, self.book_initial_container.get_by_id(book.data_id))
-		self.assertEqual(kohlrabi, self.kohlrabi_initial_container.get_by_id(kohlrabi.data_id))
+
+
+	def test_init_different_items(self):
+		reader_mock = Mock()
+		reader_mock.read_line.side_effect = [
+			"1105\t0x2\t80\t2\tbook\ta book\ta book of fairytales in English. It is open on a particular page\tThe Pied Piper of Hamelin",
+			"1106\t0x101A\t81\t3\tlamp\ta lamp\ta small lamp\t0",
+			"---",
+		]
+
+		collection = ItemCollection(reader_mock, self.location_collection)
+
+		self.assertEqual(2, len(collection.items))
+		book = collection.items["book"]
+		lamp = collection.items["lamp"]
+		self.assertIsNot(book, lamp)
+
+
+	def test_init_aliased_item(self):
+		reader_mock = Mock()
+		reader_mock.read_line.side_effect = [
+			"1042\t2002\t27\t3\tkohlrabi,cabbage\tsome kohlrabi\tsome kohlrabi, or Brassica oleracea var. gongylodes, a cabbage cultivar\t0",
+			"---",
+		]
+
+		collection = ItemCollection(reader_mock, self.location_collection)
+
+		self.assertEqual(2, len(collection.items))
+		kohlrabi = collection.items["kohlrabi"]
+		cabbage = collection.items["cabbage"]
+		self.assertIs(kohlrabi, cabbage)
+
+
+	def test_init_item_without_container(self):
+		reader_mock = Mock()
+		reader_mock.read_line.side_effect = [
+			"1076\t22802\t1007\t1\twater\twater\tRiver Amethyst water. It is cold and clear\t0",
+			"---",
+		]
+
+		collection = ItemCollection(reader_mock, self.location_collection)
+
+		self.assertEqual(1, len(collection.items))
+		self.assertTrue("water" in collection.items)
+		water = collection.items["water"]
+		self.assertIsNone(water.container)
+
+
+	def test_init_container_item(self):
+		reader_mock = Mock()
+		reader_mock.read_line.side_effect = [
+			"1002\t3\t119\t5\tbasket\ta\tbasket\ta\tlarge basket\t0",
+			"---",
+		]
+
+		collection = ItemCollection(reader_mock, self.location_collection)
+
+		self.assertEqual(1, len(collection.items))
+		self.assertTrue("basket" in collection.items)
+		basket = collection.items["basket"]
+		self.assertTrue(isinstance(basket, ContainerItem))
 
 
 if __name__ == "__main__":
