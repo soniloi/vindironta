bdfb3b4e3731ec4062e90e5b3f314b13e1418a42
soniloi <niloinso@tcd.ie>
2018-11-10 12:07:20 +0000

    Restructure arg resolution

--- adventure/argument_resolver.py
+++ adventure/argument_resolver.py
@@ -35,58 +35,56 @@ class ArgumentResolver:
 
 
 	def resolve_args(self, command, player, args):
-		if not args and not command.is_permissive():
-			player.current_command = command
-			return self.data.get_response("request_direct"), command.primary
-		return self.resolve_args_for_item(command, player, args)
-
-
-	def resolve_args_for_item(self, command, player, args):
-
-		if not command.takes_item_arg(0):
-			return self.execute(command, player, args)
-
-		item = self.data.get_item(self.get_first_arg(args))
-		if not item:
-			return self.data.get_response("reject_unknown"), self.get_first_arg(args)
-
-		return self.resolve_item_source(command, player, item, args[1:])
-
-
-	def resolve_item_source(self, command, player, item, other_args):
-
-		if command.takes_item_arg_from_inventory_only(0) and not player.is_carrying(item):
-			return self.data.get_response("reject_not_holding"), item.shortname
-
-		if command.takes_item_arg_from_location_only(0):
-			if player.is_carrying(item):
-				return self.data.get_response("reject_carrying"), item.shortname
-			if not player.is_near_item(item):
-				return self.data.get_response("reject_not_here"), item.shortname
-
-		if command.takes_item_arg_from_inventory_or_location(0) and not player.has_or_is_near_item(item):
-			return self.data.get_response("reject_not_here"), item.shortname
-
-		return self.resolve_item_switch(command, player, item, other_args)
-
-
-
-	def resolve_item_switch(self, command, player, item, switch_args):
-
+		success, response = self.resolve_arg_for_command(command, player, command.arg_infos[0], self.get_first_arg(args))
+		if not success:
+			return response
 		if command.is_switching():
-			if not item.is_switchable():
-				return self.data.get_response("reject_no_understand_instruction"), item.shortname
+			return self.execute_switching(command, player, response, args[1:])
+		return self.execute(command, player, [response])
 
-			transition_text = self.get_first_arg(switch_args)
 
-			if not transition_text in item.text_to_transition:
-				content = [item.shortname] + sorted(list(item.text_to_transition.keys()))
-				return self.data.get_response("request_switch_item"), content
+	def resolve_arg_for_command(self, command, player, arg_info, arg_input):
+		if not arg_input:
+			if arg_info.mandatory:
+				player.current_command = command
+				return False, (self.data.get_response("request_direct"), command.primary)
+			return True, ""
 
-			transition = item.text_to_transition.get(transition_text)
-			return command.handler_function(player, item, transition)
+		if not arg_info.is_item:
+			return True, arg_input
 
-		return self.execute(command, player, [item])
+		item = self.data.get_item(arg_input)
+		if not item:
+			return False, (self.data.get_response("reject_unknown"), arg_input)
+
+		if arg_info.takes_item_arg_from_inventory_only() and not player.is_carrying(item):
+			return False, (self.data.get_response("reject_not_holding"), item.shortname)
+
+		if arg_info.takes_item_arg_from_location_only():
+			if player.is_carrying(item):
+				return False, (self.data.get_response("reject_carrying"), item.shortname)
+			if not player.is_near_item(item):
+				return False, (self.data.get_response("reject_not_here"), item.shortname)
+
+		if arg_info.takes_item_arg_from_inventory_or_location() and not player.has_or_is_near_item(item):
+			return False, (self.data.get_response("reject_not_here"), item.shortname)
+
+		return True, item
+
+
+	def execute_switching(self, command, player, item, switch_args):
+
+		if not item.is_switchable():
+			return self.data.get_response("reject_no_understand_instruction"), item.shortname
+
+		transition_text = self.get_first_arg(switch_args)
+
+		if not transition_text in item.text_to_transition:
+			content = [item.shortname] + sorted(list(item.text_to_transition.keys()))
+			return self.data.get_response("request_switch_item"), content
+
+		transition = item.text_to_transition.get(transition_text)
+		return command.handler_function(player, item, transition)
 
 
 	def get_first_arg(self, args):
