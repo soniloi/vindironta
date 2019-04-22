c09f86845a5bd9821963205f3d87a6d33b7e4f24
soniloi <niloinso@tcd.ie>
2019-04-22 18:23:14 +0100

    Remove hard-coded filename from cli

--- cli/cli.py
+++ cli/cli.py
@@ -1,3 +1,5 @@
+import sys
+
 from adventure import game
 
 RESET_COLOUR = "\x1b[0m"
@@ -6,7 +8,11 @@ OUTPUT_COLOUR = "\x1b[32m"
 PROMPT = "> "
 
 if __name__ == '__main__':
-	current_game = game.Game("datafile/rucesse.dat")
+	if len(sys.argv) != 2:
+		print("Usage: {0} <filename>".format(sys.argv[0]))
+		sys.exit(1)
+
+	current_game = game.Game(sys.argv[1])
 
 	while(current_game.on):
 		request = input(INPUT_COLOUR + PROMPT)
