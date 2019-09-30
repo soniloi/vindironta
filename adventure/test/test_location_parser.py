ad31d858868ddd04f84dd27896b5f9eade7555cb
soniloi <niloinso@tcd.ie>
2019-09-30 15:01:01 +0100

    Split up location parser test

--- adventure/test/test_location_parser.py
+++ adventure/test/test_location_parser.py
@@ -12,7 +12,72 @@ class TestLocationParser(unittest.TestCase):
 		pass
 
 
-	def test_init(self):
+	def test_init_no_teleport_info(self):
+		location_inputs = json.loads(
+			"[ \
+				{ \
+					\"data_id\": 7, \
+					\"attributes\": \"70F\", \
+					\"directions\": { \
+						\"west\": 8, \
+						\"southeast\": 34, \
+						\"northwest\": 9, \
+						\"down\": 33 \
+					}, \
+					\"labels\": { \
+						\"shortname\": \"Infirm\", \
+						\"longname\": \"in the infirmary\", \
+						\"description\": \"; the room is dimly-lit, presumably with emergency lighting\" \
+					} \
+				}, \
+				{ \
+					\"data_id\": 9, \
+					\"attributes\": \"70F\", \
+					\"directions\": { \
+						\"south\": 7 \
+					}, \
+					\"labels\": { \
+						\"shortname\": \"Ward\", \
+						\"longname\": \"in a medical ward\", \
+						\"description\": \". The faint electric light is flickering on and off\", \
+						\"extended_descriptions\": [ \
+							\". There is no way out of this room\", \
+							\". There is a hole in the wall that you can escape through\" \
+						] \
+					} \
+				} \
+			]"
+		)
+
+		collection, validation = LocationParser().parse(location_inputs, {})
+
+		self.assertEqual(2, len(collection.locations))
+		self.assertTrue(7 in collection.locations)
+		self.assertTrue(9 in collection.locations)
+
+		ward_location = collection.locations[9]
+		self.assertEqual(0x70F, ward_location.attributes)
+		self.assertEqual("Ward", ward_location.shortname)
+		self.assertEqual("in a medical ward", ward_location.longname)
+		self.assertEqual(". The faint electric light is flickering on and off", ward_location.description)
+		self.assertEqual(2, len(ward_location.extended_descriptions))
+		self.assertEqual([". There is no way out of this room", ". There is a hole in the wall that you can escape through"], ward_location.extended_descriptions)
+
+		infirmary_location = collection.locations[7]
+		self.assertIsNot(ward_location, infirmary_location)
+
+		self.assertEqual(infirmary_location, ward_location.directions[Direction.SOUTH])
+		self.assertEqual(infirmary_location, ward_location.directions[Direction.OUT])
+
+		self.assertEqual(ward_location, infirmary_location.directions[Direction.NORTHWEST])
+		self.assertNotIn(Direction.NORTH, infirmary_location.directions)
+		self.assertNotIn(Direction.WEST, infirmary_location.directions)
+		self.assertNotIn(Direction.OUT, infirmary_location.directions)
+
+		self.assertFalse(validation)
+
+
+	def test_init_teleport_info_valid(self):
 		location_inputs = json.loads(
 			"[ \
 				{ \
@@ -53,63 +118,17 @@ class TestLocationParser(unittest.TestCase):
 
 		collection, validation = LocationParser().parse(location_inputs, teleport_infos)
 
-		self.assertEqual(2, len(collection.locations))
-		self.assertTrue(7 in collection.locations)
-		self.assertTrue(9 in collection.locations)
-
 		ward_location = collection.locations[9]
-		self.assertEqual(0x70F, ward_location.attributes)
-		self.assertEqual("Ward", ward_location.shortname)
-		self.assertEqual("in a medical ward", ward_location.longname)
-		self.assertEqual(". The faint electric light is flickering on and off", ward_location.description)
-		self.assertEqual(2, len(ward_location.extended_descriptions))
-		self.assertEqual([". There is no way out of this room", ". There is a hole in the wall that you can escape through"], ward_location.extended_descriptions)
-
 		infirmary_location = collection.locations[7]
-		self.assertIsNot(ward_location, infirmary_location)
-
-		self.assertEqual(infirmary_location, ward_location.directions[Direction.SOUTH])
-		self.assertEqual(infirmary_location, ward_location.directions[Direction.OUT])
-
-		self.assertEqual(ward_location, infirmary_location.directions[Direction.NORTHWEST])
-		self.assertNotIn(Direction.NORTH, infirmary_location.directions)
-		self.assertNotIn(Direction.WEST, infirmary_location.directions)
-		self.assertNotIn(Direction.OUT, infirmary_location.directions)
-
 		self.assertIs(infirmary_location, command.teleport_info[9])
 		self.assertIs(ward_location, command.teleport_info[7])
 
 		self.assertFalse(validation)
 
 
-	def test_init_invalid_teleport_info(self):
+	def test_init_teleport_info_invalid_unknown_location_ids(self):
 		location_inputs = json.loads(
 			"[ \
-				{ \
-					\"data_id\": 7, \
-					\"attributes\": \"70F\", \
-					\"directions\": { \
-						\"northwest\": 9, \
-						\"down\": 8 \
-					}, \
-					\"labels\": { \
-						\"shortname\": \"Infirm\", \
-						\"longname\": \"in the infirmary\", \
-						\"description\": \"; the room is dimly-lit, presumably with emergency lighting\" \
-					} \
-				}, \
-				{ \
-					\"data_id\": 8, \
-					\"attributes\": \"70F\", \
-					\"directions\": { \
-						\"up\": 7 \
-					}, \
-					\"labels\": { \
-						\"shortname\": \"Morgue\", \
-						\"longname\": \"in the morgue\", \
-						\"description\": \", a dim place\" \
-					} \
-				}, \
 				{ \
 					\"data_id\": 9, \
 					\"attributes\": \"70F\", \
@@ -129,34 +148,11 @@ class TestLocationParser(unittest.TestCase):
 			]"
 		)
 		command = Command(150, 0x0, [], [], ["do"], {})
-		teleport_infos = {command: {111: 9, 9: 17, 8: 8}}
+		teleport_infos = {command: {111: 9, 9: 17}}
 
 		collection, validation = LocationParser().parse(location_inputs, teleport_infos)
 
-		self.assertEqual(3, len(collection.locations))
-		self.assertTrue(7 in collection.locations)
-		self.assertTrue(9 in collection.locations)
-
-		ward_location = collection.locations[9]
-		self.assertEqual(0x70F, ward_location.attributes)
-		self.assertEqual("Ward", ward_location.shortname)
-		self.assertEqual("in a medical ward", ward_location.longname)
-		self.assertEqual(". The faint electric light is flickering on and off", ward_location.description)
-		self.assertEqual(2, len(ward_location.extended_descriptions))
-		self.assertEqual([". There is no way out of this room", ". There is a hole in the wall that you can escape through"], ward_location.extended_descriptions)
-
-		infirmary_location = collection.locations[7]
-		self.assertIsNot(ward_location, infirmary_location)
-
-		self.assertEqual(infirmary_location, ward_location.directions[Direction.SOUTH])
-		self.assertEqual(infirmary_location, ward_location.directions[Direction.OUT])
-
-		self.assertEqual(ward_location, infirmary_location.directions[Direction.NORTHWEST])
-		self.assertNotIn(Direction.NORTH, infirmary_location.directions)
-		self.assertNotIn(Direction.WEST, infirmary_location.directions)
-		self.assertNotIn(Direction.OUT, infirmary_location.directions)
-
-		self.assertEqual(3, len(validation))
+		self.assertEqual(2, len(validation))
 
 		validation_line_0 = validation[0]
 		self.assertEqual("Unknown source location id {0} for teleport command {1} \"{2}\". This command will be unreachable.", validation_line_0.template)
@@ -168,10 +164,38 @@ class TestLocationParser(unittest.TestCase):
 		self.assertEqual(Severity.ERROR, validation_line_1.severity)
 		self.assertEqual((17, 150, "do"), validation_line_1.args)
 
-		validation_line_2 = validation[2]
-		self.assertEqual("Source id and destination id {0} are the same for teleport command {1} \"{2}\".", validation_line_2.template)
-		self.assertEqual(Severity.WARN, validation_line_2.severity)
-		self.assertEqual((8, 150, "do"), validation_line_2.args)
+
+	def test_init_teleport_info_invalid_matching_location_ids(self):
+		location_inputs = json.loads(
+			"[ \
+				{ \
+					\"data_id\": 9, \
+					\"attributes\": \"70F\", \
+					\"directions\": { \
+						\"south\": 7 \
+					}, \
+					\"labels\": { \
+						\"shortname\": \"Ward\", \
+						\"longname\": \"in a medical ward\", \
+						\"description\": \". The faint electric light is flickering on and off\", \
+						\"extended_descriptions\": [ \
+							\". There is no way out of this room\", \
+							\". There is a hole in the wall that you can escape through\" \
+						] \
+					} \
+				} \
+			]"
+		)
+		command = Command(150, 0x0, [], [], ["do"], {})
+		teleport_infos = {command: {9: 9}}
+
+		collection, validation = LocationParser().parse(location_inputs, teleport_infos)
+
+		self.assertEqual(1, len(validation))
+		validation_line = validation[0]
+		self.assertEqual("Source id and destination id {0} are the same for teleport command {1} \"{2}\".", validation_line.template)
+		self.assertEqual(Severity.WARN, validation_line.severity)
+		self.assertEqual((9, 150, "do"), validation_line.args)
 
 
 if __name__ == "__main__":
